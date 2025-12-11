14448d9e4f767b2b1834aaef8da5ff8813bba1e1
Romeo Enso <romeo.enso@connexionltd.com>
2025-12-11 14:49:27 +1100

    trigger deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,8 @@ import { app } from "./app";
 import { connectDb } from "./config/db";
 import { config } from './config/env';
 
+console.log('GH Actions deployment test');
+
 connectDb().then(() => {
   app.listen(config.port, () => 
    console.log(`API running on port ${config.port}`));
